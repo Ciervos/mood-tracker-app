f71b1cd911fcee2d9c1f4a6e3b2bd31dbf76f706
Ciervos <mlujafo@gmail.com>
2022-02-11 19:19:52 -0300

    add calendar library

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Calendar from 'react-native-swipe-calendar';
 
 export default function App() {
   return (
