439d429567edb8f4d259fc3850b182fa614e000a
Ciervos <mlujafo@gmail.com>
2022-02-28 19:43:19 -0300

    más pruebas de calendarios librerias

--- screens/MainScreen/MainScreen.js
+++ screens/MainScreen/MainScreen.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import {Text, View,Image} from 'react-native';
 import styles from './styles';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar } from 'react-native-calendario';
 import colors from '../../constants/colors';
 
 
@@ -11,42 +11,19 @@ const MainScreen = () => {
   return (
     <View style={styles.container}>
     <Text style={styles.generaltext}>Principal</Text>
-    {/* Buscar nueva libreria de calendario/implementar de cero, fallas provenientes de la misma. Se necesita reiniciar app para cambiar colores. Not good */}
-
-     <Calendar
-     markingType={'custom'}
-     style={{
-      borderWidth: 1,
-      borderColor: colors.color3,
-      height: 360
-    }}
-    // Specify theme properties to override specific styles for calendar parts. Default = {}
-    theme={{
-      backgroundColor: colors.color3,
-      calendarBackground: colors.maincolor,
-      textSectionTitleColor: '#b6c1cd',
-      textSectionTitleDisabledColor: '#d9e1e8',
-      selectedDayBackgroundColor: '#00adf5',
-      selectedDayTextColor: 'yellow',
-      todayTextColor: 'blue',
-      dayTextColor: 'orange',
-      textDisabledColor: '#d9e1e8',
-      dotColor: '#00adf5',
-      selectedDotColor: '#ffffff',
-      arrowColor: colors.color2,
-      disabledArrowColor: '#d9e1e8',
-      monthTextColor: colors.color2,
-      indicatorColor: 'blue',
-      textDayFontFamily: 'monospace',
-      textMonthFontFamily: 'monospace',
-      textDayHeaderFontFamily: 'monospace',
-      textDayFontWeight: '300',
-      textMonthFontWeight: 'bold',
-      textDayHeaderFontWeight: '300',
-      textDayFontSize: 16,
-      textMonthFontSize: 16,
-      textDayHeaderFontSize: 16
-    }}/>
+    <Calendar numberOfMonths={1} theme={{
+      monthTitleTextStyle: {
+      color: colors.color3,
+      fontWeight: '300',
+      fontSize: 16,
+      fontFamily: 'Lato',
+    },
+    weekColumnsContainerStyle: {
+     width: 400,
+    },
+    weekColumnStyle: {
+      paddingVertical: 5,
+    },}}/>
     </View>
     
   );
