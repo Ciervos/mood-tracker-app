d040d203779bc6623441e8aa7edb98def914a211
Ciervos <mlujafo@gmail.com>
2022-03-01 19:34:32 -0300

    core emotions

---
+++ data/emotions.js
@@ -0,0 +1,40 @@
+
+export const EMOTIONS = [
+    {
+        id: '1',
+        emotion: 'Feliz',
+        color: '#fff44f'
+      },
+      {
+        id: '2',
+        emotion: 'Triste',
+        color: '#3153ac'
+      },
+      {
+        id: '3',
+        emotion: 'Sorprendido',
+        color: '#6247aa'
+      },
+      {
+        id: '4',
+        emotion: 'Mal',
+        color: '#30736d'
+      },
+      {
+        id: '5',
+        emotion: 'Asustado',
+        color: '#d9873c'
+      },
+      {
+        id: '6',
+        emotion: 'Enojado',
+        color: '#c5000a'
+      },
+      {
+        id: '7',
+        emotion: 'Disgustado',
+        color: '#494f5e'
+      }
+
+
+]
